23db7657c171a035c9d66c4bdff9fe8c0fc445b6
naeliofreires <naeliofreires@gmail.com>
2019-09-17 21:04:14 -0300

    interpolate

--- src/index.js
+++ src/index.js
@@ -8,62 +8,30 @@ import {SafeAreaView, StyleSheet, View, Animated} from 'react-native';
 
 export default function App() {
   const [ballY] = useState(new Animated.Value(0));
-  const [ballX] = useState(new Animated.Value(0));
 
   useEffect(() => {
-    // linear
-    // Animated.timing(ballY, {
-    //   toValue: 200,
-    //   duration: 500,
-    // }).start();
-
-    // linear com efeito elastico
-    // Animated.spring(ballY, {
-    //   toValue: 300,
-    //   bounciness: 20,
-    // }).start();
-
-    // Animated.decay(ballY, {
-    //   velocity: 1,
-    // }).start();
-
-    // Animated.stagger(value,[]);
-
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(ballY, {
-          toValue: 200,
-          duration: 500,
-        }),
-
-        Animated.delay(100),
-
-        Animated.timing(ballX, {
-          toValue: 200,
-          duration: 500,
-        }),
-
-        Animated.delay(100),
-
-        Animated.timing(ballY, {
-          toValue: 0,
-          duration: 500,
-        }),
-
-        Animated.delay(100),
-
-        Animated.timing(ballX, {
-          toValue: 0,
-          duration: 500,
-        }),
-      ]),
-    ).start();
-  }, [ballX, ballY]);
+    Animated.timing(ballY, {
+      toValue: 300,
+      duration: 500,
+    }).start();
+  }, [ballY]);
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
-        <Animated.View style={[styles.ball, {top: ballY, left: ballX}]} />
+        <Animated.View
+          style={[
+            styles.ball,
+            {
+              top: ballY,
+              opacity: ballY.interpolate({
+                inputRange: [0, 300],
+                outputRange: [1, 0.2],
+                extrapolate: 'clamp',
+              }),
+            },
+          ]}
+        />
       </View>
     </SafeAreaView>
   );
@@ -81,3 +49,49 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
 });
+
+/*
+
+  => linear com efeito elastico
+
+  Animated.spring(ballY, {
+    toValue: 300,
+    bounciness: 20,
+  }).start();
+
+  Animated.decay(ballY, {
+    velocity: 1,
+  }).start();
+
+  Animated.stagger(value,[]);
+
+  Animated.loop(
+    Animated.sequence([
+      Animated.timing(ballY, {
+        toValue: 200,
+        duration: 500,
+      }),
+
+      Animated.delay(100),
+
+      Animated.timing(ballX, {
+        toValue: 200,
+        duration: 500,
+      }),
+
+      Animated.delay(100),
+
+      Animated.timing(ballY, {
+        toValue: 0,
+        duration: 500,
+      }),
+
+      Animated.delay(100),
+
+      Animated.timing(ballX, {
+        toValue: 0,
+        duration: 500,
+      }),
+    ]),
+  ).start();
+*/
